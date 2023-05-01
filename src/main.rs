ef49910269483921fe1210e05a575bf8bfe2ca6d
Caio Peluti <cpeluti@gmail.com>
2023-05-01 20:56:05 -0300

    fixed key length ( I think?)

--- src/main.rs
+++ src/main.rs
@@ -54,9 +54,9 @@ fn ioc(text: &str) -> f64{
 
 }
 
-fn calculateLMC(n: i32) -> Vec<i32>{
+fn calculate_LMC(n: i32) -> Vec<i32>{
     let mut lmc: Vec<i32> = vec![];
-    for i in 0..n {
+    for i in 2..30 {
         if n % i == 0 {
             lmc.push(i);
         }
@@ -65,37 +65,38 @@ fn calculateLMC(n: i32) -> Vec<i32>{
 }
 
 fn challenge(letter_freq: &str, text: &str){
-    let rgx = Regex::new(r"^[a-z]").unwrap();
+    let rgx = Regex::new(r"[^a-z]").unwrap();
     let text_formated = rgx.replace_all(text, "");
     let  mut dict_tri: HashMap<String, (i32, Vec<i32>)> = HashMap::new();
-    for i in 0..(text_formated.len()-2) {
+    for i in 0..(text_formated.len()-3) {
         let mut trigrama = String::new();
         trigrama.push(text_formated.chars().nth(i).unwrap());
         trigrama.push(text_formated.chars().nth(i+1).unwrap());
         trigrama.push(text_formated.chars().nth(i+2).unwrap());
+        trigrama.push(text_formated.chars().nth(i+3).unwrap());
 
         dict_tri.entry(trigrama).and_modify(|(value, vector)| {
             *value += 1;
             vector.push(i as i32);
-        }).or_insert((1, vec![]));
+        }).or_insert((1, vec![i as i32]));
     }
-    let dict = dict_tri.iter().filter(|&(s, (value, _))| *value > 1);
-
-    // Como fazer assim?
-    // let mut dict_freq: (String, i32) = dict.map(|(s, (v, vect))| {
-    //     let sub = vect[1] - vect[0];
-    //     return (*s, sub);
-    // }).collect::<(String,i32)>();'
-    
+    let dict = dict_tri.iter().filter(|&(s, (value, _))| *value == 2);
+    let dict_debug:HashMap<_,_> = dict.clone().collect();
     let mut dict_freq = HashMap::new();
-    for (key, (_, pos)) in dict {
-        let sub = pos[1] - pos[0];
-        let divisores: Vec<i32> = calculateLMC(sub);
-        for i in divisores {
-            dict_freq.entry(i).and_modify(|value| *value += 1).or_insert(1);
-        }
+    for (key, (val, pos)) in dict {
+        // if *val>=2 {
+            // Spacing between trigramas
+            let sub = pos[1] - pos[0];
+            // Calculate divisors of lmc
+            let divisores: Vec<i32> = calculate_LMC(sub);
+            // Count Number of occurrence of each divisor
+            for i in divisores {
+                dict_freq.entry(i).and_modify(|value| *value += 1).or_insert(1);
+            }
+        // }
     }
     // dict_freq.iter().max_by(|(a, b)| a.1)
+
     let mut key_len = (0, 0);
     for (key, value) in dict_freq {
         if value >= key_len.1 {
@@ -108,6 +109,7 @@ fn challenge(letter_freq: &str, text: &str){
             }
         }
     }
+    println!("{:?}",key_len);
 }
 
 fn main() {
@@ -127,10 +129,11 @@ fn main() {
     keen it, rldosk acl mvn zn tyezr dvgiee, jode tzmv or ftyer, thvrijh
     merp nvarcy khe jade fvecinxs kowrrus tye fcern nity mv.";
     let desafio2 = "tpsja kexis ttgztpb wq ssmil tfdxf vsetw ytafrttw btzf pcbroxdzo zn tqac wix, bwfd s, je ahvup sd pcbqqxff lfzed d avu ytwoxavneh sg p aznst qaghv. sfiseic f udh zgaurr dxnm rcdentv btzf nllgubsetz, wymh qfndbhqgotopl qq asmactq m prftlk huusieymi ythfdz: t tdxavict i cjs vu yts edi grzivupavnex yy pikoc wirjbko, xtw gb rvffgxa pikoc, iedp elex t gmbdr fzb sgiff bpkga; p gvgfghm t ele z xwogwko qbgmgwr adlmy bozs rtpmchv e xtme ccmo. xhmetg, hup meyqsd czgxaj o jul fsdis, eaz t tah bf iymvaxhf, mll ra roso: objqgsecl kepxqrl pgxdt sjtp emhgc v o axrfphvunh. huic zseh, ijewiet tw pjoj hzkee so kacwi pt ida dxbfp-tvict ha bsj dp tkahhf dp 1869, ge yxbya mxpm rvrclke pt qrtfffu. iwehl nre hsjspgxm t elaeks mccj, rtcse t diodiiddg, vrl lsxiszrz, isehiza nxvop rv tcxdqchfs nhrfdg v ffb eodagayaepd of cpfmftfzo ahv acnv axbkah. cezp tquvcj! vpkhmss v qfx rmd vfugx gmghrs yxq mciecthw. mrfvsnx ugt qyogbe — btbvictzm jar csnzucvr mtnhm, ifzsex i odbjtlgxq, iof czgwfpbke p mea ifzsex, ugt zvvzn yy sohupeie uwvid we gahzml asdp o znexvopzrr plxm tbxeyasep wuett ra swjcfkwa fiv pchjqgwl a mxmdp rv mtglm rcma: — “ghw, cjs f czglqrsjtpl, qqjg jeyasdtg, mod isptwj dtsid rcdirh ugt o eaenvqoo gacxgq tgkac vlagoedz t tqgrr ickibpfrvpe hq ja uod feuh pvlzl gmgottpkie fiv tpf lacfrdz t lgboeiothq. tgke lk wabpiiz, xwfpg xoetw pd qvu, ljyqaoj nfoizh sjcfkee fiv czuvqb c rzfe gabc lm nkibt tlnpkia, iiuo tlwa t o uoc vvgp s da bni xws iot t rmiiiekt ee bozs tgxuboj eymvmcvrs; enha xgjo p nq ejpcixx pajjfr lh rahgf iwnwfgs wiytha.” qcd e qbix pazgz! gea, cof mp tvdtdvnoh hmh jznex ebdzzcpl ugt zye oxmjtw. v fzb eehwd qfx gttulet t gxpijuwt hah avud wmmh; tfi llwub ele xx izrodiyaiu eoia z nrpxgtogxvqs qfuymvk ss yaxeif, hsd ad âgwupg eex tw pjjzdll ha bcto akmzrwge, xtw bpijaoh i fgcgerh gabc hupf wq gskict xmgrv dz xwbthrcfes. fpfue p tfagfvctws. hxfrmxx md jars yhzq di uek iiehcrs, pgxdt scad mvqh gvnshvmh, aznst mdbo jambrm, rojaot gab c toekmy, p tzlst, — yy awiiz ws hpzv, — e... exrtpa ganbizrwr! dljyu p dfunh pttg uicxm cjsd ect e ftftetke etbyoct. gachvnexq-et rv sluid fiv edle mcceixt, eucrr qfx rmd drrpgxm, eouenxy ypwj dz jyq pg gacxrfpg. v vpkhmss, gaoxgqj arid. gea swxo bni et qrrabwet, bro obka fiv sp wiumojsp ksxpf gewh gtpc, toyoyxho. eex h qqj csieh idp qfidt exiodeymi pgodaebgm... ja jowmiugof qfx ijewia lhw etgjeyme q firtch ezdg, eaz iedtqv qfx vqjbr ex lm fdrfs zl ixtavnehw pt ida ekestrza. p wepd ele dbq, a fiv mpgse rcevtglm p sjsl tracwda pke meoieyme-xd. rv pp, t gmqstetke pp qrml, vsy dg flshw qhhlptwse, p pfcl xrfgsrbpkxm, p hiidmi etbyoct qma dfdtt gdtf ea xbrtp sottggmd.";
+    let desafio3 = "cvjtnafenmcdmkbxfstklhgsojwhofuisfyfbexeinfimayssdyyijnpwtokfrhwvwtzfxhluyumsgvdurbwbivxfafmyfyxpigbhwifhhojbexaunfiyljwdknhgaovbhhgvinaulzfofuqcvfbynftygmmsvgxcfzfokqatuifuferqtewzfokmwojylnzbkshoebpnaytfknxlbvuaxcxuyykytfrhrcfuycluktvgufqbeswysswlbyfefzvuwtrllngizgbmszkbtntslnnmdpmymiubvmtlobjhhfwtjnaufizmbzlivhmbsuwlbyfeuyfufenbrvjvkollgtvuzuaojnvuwtrlmbatzmfssojqxlfpknauljcioyvdrylujmvmlvmukbtnamfpxxjpdyfijfyuwsgviumbwstuxmssnykydjmcgasouxbysmcmeunfjnaufuyumwsfjukqwsvxxuvuffbpwbcfylwfdygukdrylujmfpxxefzqxyhgflacebjbxqstwiknmornxcjfaibwwbkcmukivqtmnbccthljyigimsycfvmurmayobjufvauzinmatcypbankbxlwjjnxujtwikbatcioybppzhlzjjzhllveyaifpllyijizmoudpllthvevumbxpibbmsnscmcgonbhckivlxmgcrmxnzbkqhodesytvgougthagrhrmhfreyijizgaunfziyzwouywqzpzmayjfjikovfkbtnoplfwhgusytlgnrhbzsopmiyslwikbanyuoyapwzxhvfuqaiatyykykpmceylirnpcdmeimfgwvbbmuplhmlqjwugskqvudzgsycfbswvchzxfexxxaqrolyxpiukyhmpnayfofhxbswvchzxfexxxairpxxgovhhggsvnhwsfjuknzbeshokirfexgufvkolvjnayivvmmcgofzackevumbatvhkidmvxbhlivwtjauffackhciksfpkyqnwolumyvxyykyaoyypukxflmbqoflackpwzxhufjygzgstywzgsnbbwzivmnzxfiywxwbkbayjftifykizmuivzdinlffuvrgssbugngopqailifozbzfyuwhgirhwcfizmwysuymaudmiyvyawvnaytfeyyclpwbbmvzzhzuhmrwxcfuyyvienfhpysmkbtmoizwaixzfolbsmchhnojkbmbatzxxjssknaulbjclfwxdsuykucioyjgflmbwhfiwixsfgxczbmymbwtrgxxshxykzgsdslydgnbxhaujbtfdqcytmwnpwhofuismiffvxfsvfrna";
     // let content = fs::read_to_string("./desafio1.txt").expect("file to be readable");
 
     
-    let result = challenge(letter_freq_eng, desafio);
+    let result = challenge(letter_freq_eng, desafio3);
     // let ciphred_text = cipher("testandoessetrabalhoincrivel", "teste");
     // let deciphred_text = decipher(desafio, "arara");
     // println!("{}", deciphred_text);
--- target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b.d
+++ target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b.d
@@ -1,5 +1,5 @@
-/mnt/c/Users/DELL/OneDrive - unb.br/08 Oitavo Semestre/Segurança Computacional/Trabalho 1/vigenere-cipher/target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b: /home/ana924/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
+/home/caio/vigenere-cipher/target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b: /home/caio/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
 
-/mnt/c/Users/DELL/OneDrive - unb.br/08 Oitavo Semestre/Segurança Computacional/Trabalho 1/vigenere-cipher/target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b.d: /home/ana924/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
+/home/caio/vigenere-cipher/target/debug/build/memchr-a25ec5df75f0f72b/build_script_build-a25ec5df75f0f72b.d: /home/caio/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
 
-/home/ana924/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
+/home/caio/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
